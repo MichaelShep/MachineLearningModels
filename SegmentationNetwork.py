dd383ebd9c4fa0136ac454c5208488fc51adf272
MichaelShep <mgs538@york.ac.uk>
2021-11-11 16:42:51 +0000

    Setup all the network layers

--- SegmentationNetwork.py
+++ SegmentationNetwork.py
@@ -11,17 +11,18 @@ from typing import List
 def extract_element_from_sublist(main_list: List[torch.Tensor], index: int) -> List[torch.Tensor]:
   return [item[index] for item in main_list]
 
-''' Creates and display a matplotlib display displaying all the output maps created for an image
+''' Creates and display a matplotlib display displaying some of the output maps created for an image
 '''
 def display_outputs(input_tensor: torch.Tensor, output: str) -> None:
+  num_outputs_to_display = len(output) if len(output) < 10 else 10
   fig = plt.figure('Output Channels', figsize=(20, 1))
 
-  fig.add_subplot(1, len(output) + 1, 1)
+  fig.add_subplot(1, num_outputs_to_display + 1, 1)
   plt.imshow(input_tensor.permute(1, 2, 0))
   plt.axis('off')
 
-  for i in range(1, len(output) + 1):
-    fig.add_subplot(1, len(output) + 1, i + 1)
+  for i in range(1, num_outputs_to_display + 1):
+    fig.add_subplot(1, num_outputs_to_display + 1, i + 1)
     plt.imshow(output[i - 1])
     plt.axis('off')
 
@@ -38,14 +39,114 @@ class SegmentationNetwork(nn.Module):
   def __init__(self):
     super(SegmentationNetwork, self).__init__()
 
-    #Create the layers for the network
-    self.conv1 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, stride=1, padding=1)
+    #Create the layers for the network - Creating an architecture very similar to that used in U-Net
+
+    #Define reLU activation which will be used as activation for all Conv layers in network
+    self.relu = nn.ReLU()
+
+    #Define max pool layer with 2x2 filter and 2 stride
+    self.max_pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
+
+    #Create all the conv layers for the contracting path with differing values of in and out channels
+    self.conv364 = self._create_conv_layer(3, 64)
+    self.conv6464 = self._create_conv_layer(64, 64)
+    self.conv64128 = self._create_conv_layer(64, 128)
+    self.conv128128 = self._create_conv_layer(128, 128)
+    self.conv128256 = self._create_conv_layer(128, 256)
+    self.conv256256 = self._create_conv_layer(256, 256)
+    self.conv256512 = self._create_conv_layer(256, 512)
+    self.conv512512 = self._create_conv_layer(512, 512)
+    self.conv5121024 = self._create_conv_layer(512, 1024)
+    self.conv10241024 = self._create_conv_layer(1024, 1024)
+
+    #Create the upsample layer which increases the size of the input by a factor of 2
+    self.upsample = torch.nn.Upsample(scale_factor=2)
+
+    #Create all the upsample and conv layers for the expanding path of the network
+    self.conv1024512 = self._create_conv_layer(1024, 512)
+    self.conv512256 = self._create_conv_layer(512, 256)
+    self.conv256128 = self._create_conv_layer(256, 128)
+    self.conv12864 = self._create_conv_layer(128, 64)
+    self.conv641 = self._create_conv_layer(64, 1)
+
+  ''' Creates a new convolution layer with some default parameters that are used for this network
+  '''
+  def _create_conv_layer(self, in_chan, out_chan, kernal_size=3, stride=1, padding=1):
+    return nn.Conv2d(in_channels=in_chan, out_channels=out_chan, kernel_size=kernal_size, stride=stride, padding=padding)
+
+  ''' Passes an input tensor through all the contracting layers of the network
+  '''
+  def _perform_contracting_path(self, x: torch.Tensor) -> torch.Tensor:
+    x = self.conv364(x) #3 input Channels, 64 output channels, 1024x1024 tensor size
+    x = self.relu(x) 
+    x = self.conv6464(x) #64 input Channels, 64 output channels, 1024x1024 tensor size
+    x = self.relu(x)
+
+    x = self.max_pool(x) #1024x1024 reduced to 512x512
+
+    x = self.conv64128(x) #64 input channels, 128 output channels, 512x512 tensor size
+    x = self.relu(x)
+    x = self.conv128128(x) #128 input channels, 128 output channels, 512x512 tensor size
+    x = self.relu(x)
+
+    x = self.max_pool(x) #512x512 reduced to 256x256
+
+    x = self.conv128256(x) #128 input channels, 256 output channels, 256x256 tensor size
+    x = self.relu(x)
+    x = self.conv256256(x) #256 input channels, 256 output channels, 256x256 tensor size
+    x = self.relu(x)
+
+    x = self.max_pool(x) #256x256 reduced to 128x128
+
+    x = self.conv256512(x) #256 input channels, 512 output channels, 128x128 tensor size
+    x = self.relu(x)
+    x = self.conv512512(x) #512 input channels, 512 output channels, 128x128 tensor size
+    x = self.relu(x)
+    
+    x = self.max_pool(x) #128x128 reduced to 64x64
+
+    x = self.conv5121024(x) #512 input channels, 1024 output channels, 64x64 tensor size
+    x = self.relu(x)
+    x = self.conv10241024(x) #1024 input channels, 1024 output channels, 64x64 tensor size
+    x = self.relu(x)
+
+    return x
+
+  ''' Takes an input that has already been passed through the contracting part of the network and expands it
+  '''
+  def _perform_expanding_path(self, x: torch.Tensor) -> torch.Tensor:
+    x = self.upsample(x) #Upsample from 64x64 to 128x128
+
+    x = self.conv1024512(x) #1024 input channels, 512 output channels, 128x128 tensor size
+    x = self.relu(x)
+    x = self.conv512512(x) #512 input channels, 512 output channels, 128x128 tensor size
+    x = self.relu(x)
+
+    x = self.upsample(x) #Upsample from 128x128 to 256x256
+
+    x = self.conv512256(x) #512 input channels, 256 output channels, 256x256 tensor size
+    x = self.relu(x)
+    x = self.conv256256(x) #256 input channels, 256 output channels, 256x256 tensor size
+    x = self.relu(x)
+
+    x = self.upsample(x) #Upsample from 256x256 to 512x512
+
+    x = self.conv256128(x) #256 input channels, 128 output channels, 512x512 tensor size
+    x = self.relu(x)
+    x = self.conv12864(x) #128 input channels, 64 output channels, 512x512 tensor size
+    x = self.relu(x)
+
+    x = self.conv641(x) #64 input channels, 1 output channel, 512x512 tensor size
+
+    return x
 
   ''' Performs a forward pass through the network for some data
       Method should not be called directly as only gets called as part of the lifecycle of the network
   '''
   def forward(self, x: torch.Tensor) -> torch.Tensor:
-    x = self.conv1(x)
+    x = self._perform_contracting_path(x)
+    x = self._perform_expanding_path(x)
+
     return x
 
 if __name__ == '__main__':
@@ -54,6 +155,7 @@ if __name__ == '__main__':
   image_directory = os.path.join(dataset_directory, 'CelebA-HQ-img')
   features_directory = os.path.join(dataset_directory, 'CelebAMask-HQ-mask-anno')
 
+  #Note: Images in this dataset are 1024 x 1024
   dataset = ImageDataset(image_directory, features_directory)
   train_data = dataset.load_data(1, 1)
 
@@ -63,4 +165,5 @@ if __name__ == '__main__':
   network = SegmentationNetwork()
   output = network(train_data_input)
 
+  print('Output Shape:', output.shape)
   display_outputs(train_data_input[0], output.detach()[0])
