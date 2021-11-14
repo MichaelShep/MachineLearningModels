4b8b76055a7f4dee78b908b39feb17ce123d1286
MichaelShep <mgs538@york.ac.uk>
2021-11-14 11:09:58 +0000

    Made so that our data can be split into random training and testing sets and put into a Pytorch Dataloader

--- DataSet.py
+++ DataSet.py
@@ -20,6 +20,8 @@ class ImageDataset:
     self._feature_map_dir = feature_map_dir
     self._tensor_transform = transforms.ToTensor()
 
+    self._load_data()
+
   ''' Converts a full file path into just the name of the file
   '''
   def _get_file_name_string(self, path: str) -> str:
@@ -29,7 +31,7 @@ class ImageDataset:
 
   ''' Loads the dataset and stores a list of all the input image locations and their relevant output maps
   '''
-  def load_data(self) -> None:
+  def _load_data(self) -> None:
     #Get the names of all the files from the image and mask directorys (other than hidden files)
     image_files = glob.glob(os.path.join(self._img_dir, '*.jpg'), recursive=True)
     map_files = glob.glob(os.path.join(self._feature_map_dir, '*', '*.png'), recursive=True)
@@ -38,8 +40,10 @@ class ImageDataset:
     for i in range(len(image_files)):
       files_dictionary[int(self._get_file_name_string(image_files[i]))] = []
 
+    #For now, only load the hair map as the output map - others will be added later
     for i in range(len(map_files)):
-      files_dictionary[int(self._get_file_name_string(map_files[i]).split('_')[0])].append(map_files[i])
+      if('hair' in self._get_file_name_string(map_files[i])):
+        files_dictionary[int(self._get_file_name_string(map_files[i]).split('_')[0])].append(map_files[i])
 
     self.ids = list(sorted(files_dictionary.items()))
 
@@ -53,13 +57,8 @@ class ImageDataset:
     image_number = self.ids[idx][0]
     input_image = self._tensor_transform(Image.open(os.path.join(self._img_dir, str(image_number) + '.jpg')))
 
-    #For now, only return the hair map as output
-    output_elements = self.ids[idx][1]
-
-    for i in range(len(output_elements)):
-      if 'hair' in self._get_file_name_string(output_elements[i]):
-        output_image = self._tensor_transform(Image.open(output_elements[i]))
-      else:
-        output_image = None
+    #For now, output only ever has 1 element as will always be the hair map
+    output_element = self.ids[idx][1][0]
+    output_image = self._tensor_transform(Image.open(output_element))
 
     return (input_image, output_image)
--- Main.py
+++ Main.py
@@ -2,21 +2,31 @@
 
 import sys
 import os.path
-from DataLoader import ImageDataset
+
+from torchvision import transforms
+
+from DataSet import ImageDataset
 from SegmentationNetwork import SegmentationNetwork
 from Training import Training
 import Helper
+from torch.utils.data import DataLoader, random_split
 
 if __name__ == '__main__':
+    NUM_TRAINING_EXAMPLES = 50
+    MINI_BATCH_SIZE = 1
+
     current_directory = sys.path[0]
     dataset_directory = os.path.join(os.path.split(current_directory)[0], 'CelebAMask-HQ')
     image_directory = os.path.join(dataset_directory, 'CelebA-HQ-img')
     features_directory = os.path.join(dataset_directory, 'CelebAMask-HQ-mask-anno')
 
-    #Note: Images in this dataset are 1024 x 1024
+    #Load all image and map data and split it randomly into testing and training data
     dataset = ImageDataset(image_directory, features_directory)
-    dataset.load_data()
-    print(dataset[4888])
+    train_data, test_data = random_split(dataset.ids, [NUM_TRAINING_EXAMPLES, len(dataset.ids) - NUM_TRAINING_EXAMPLES])
+    train_loader = DataLoader(train_data, batch_size=MINI_BATCH_SIZE, shuffle=True, num_workers=1)
+    test_loader = DataLoader(test_data, batch_size=MINI_BATCH_SIZE, shuffle=False, num_workers=1)
+
+    input_image_idx, output_map_name = next(iter(train_loader))
 
     #model = SegmentationNetwork()
     #model_training = Training(model, train_data)
