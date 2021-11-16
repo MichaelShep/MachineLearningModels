32f85eb7bf2e90f3cfd866d95e2638ec953bcdda
Michael <mgs538@york.ac.uk>
2021-11-16 12:02:06 +0000

    Moved network and tensors to GPU for compatible devices - improves speeds hugely but means can only use a small mini batch size due to memory

--- Main.py
+++ Main.py
@@ -5,6 +5,7 @@ import os.path
 from CelebADataset import CelebADataset
 from SegmentationNetwork import SegmentationNetwork
 from Training import Training
+import torch
 
 if __name__ == '__main__':
     current_directory = sys.path[0]
@@ -13,7 +14,10 @@ if __name__ == '__main__':
     features_directory = os.path.join(dataset_directory, 'CelebAMask-HQ-mask-anno')
 
     #Load all image and map data and split it randomly into testing and training data
-    dataset = CelebADataset(image_directory, features_directory)
-    model = SegmentationNetwork()
+    dataset = CelebADataset(image_directory, features_directory)#
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    if device == 'cuda':
+        torch.cuda.empty_cache()
+    model = SegmentationNetwork().to(device=device)
     model_training = Training(model, dataset)
     model_training.train()
--- Training.py
+++ Training.py
@@ -9,9 +9,9 @@ from typing import Tuple
 import time
 
 class Training():
-  _MINI_BATCH_SIZE = 1
+  _MINI_BATCH_SIZE = 2
   _NUM_EPOCHS = 1
-  _NUM_TRAINING_EXAMPLES = 20000
+  _NUM_TRAINING_EXAMPLES = 15000
 
   ''' Splits the data into training and testing data and sets up data loader so data can be dealt with in
       batches
@@ -21,8 +21,8 @@ class Training():
     self._dataset = dataset
     self._training_examples, self._testing_examples = dataset.get_train_test_split(self._NUM_TRAINING_EXAMPLES)
 
-    self._training_loader = DataLoader(self._training_examples, batch_size=self._MINI_BATCH_SIZE, shuffle=True, num_workers=1)
-    self._testing_loader = DataLoader(self._testing_examples, batch_size=self._MINI_BATCH_SIZE, shuffle=False, num_workers=1)
+    self._training_loader = DataLoader(self._training_examples, batch_size=self._MINI_BATCH_SIZE, shuffle=True, num_workers=2)
+    self._testing_loader = DataLoader(self._testing_examples, batch_size=self._MINI_BATCH_SIZE, shuffle=False, num_workers=2)
 
     #Using Per Pixel Cross Entropy Loss for our loss and Stochastic Gradient Descent for our optimiser
     self._loss_func = nn.CrossEntropyLoss(reduction='none')
@@ -39,22 +39,25 @@ class Training():
       input_values.append(element[0])
       output_values.append(element[1])
 
-    return (torch.stack(input_values), torch.stack(output_values))
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    return (torch.stack(input_values).to(device), torch.stack(output_values))
 
 
   ''' Performs the actual training using our training data and model
   '''
   def train(self) -> None:
-    for _ in range(self._NUM_EPOCHS):
+    for epoch in range(self._NUM_EPOCHS):
       for i, data_indexes in enumerate(self._training_loader):
-        if i == 0:
-          input_data, output_data = self._get_data_for_indexes(data_indexes)
-          print('Starting to run model')
-          start_time = time.time()
-          model_output = self._model(input_data)
-          print('Time taken to run on single mini batch of size', self._MINI_BATCH_SIZE, ':', (time.time() - start_time), 'seconds')
-  
-          '''Loss is not currently working correctly - need to find a better way to handle this
-          torch.set_printoptions(profile='full')
-          loss = self._loss_func(model_output[0], output_data[0])
-          print('Loss value of:', loss)'''
+        input_data, output_data = self._get_data_for_indexes(data_indexes)
+        start_time = time.time()
+        model_output = self._model(input_data)
+        print('Epoch:', epoch, 'Batch:', i, 'Time Taken:', (time.time() - start_time), 'seconds')
+
+        #Clear all unneeded memory - without this will get a memory error
+        del input_data, model_output
+        torch.cuda.empty_cache()
+
+        '''Loss is not currently working correctly - need to find a better way to handle this
+        torch.set_printoptions(profile='full')
+        loss = self._loss_func(model_output[0], output_data[0])
+        print('Loss value of:', loss)'''
